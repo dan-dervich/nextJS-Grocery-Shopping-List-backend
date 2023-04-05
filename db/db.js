281917e47272e553519a7a9df1c0b5cecb340261
dan-dervich <dandervich@hotmail.com>
2023-04-05 16:57:53 -0300

    fix: mongodb connection URI error

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connect = async () => {
     try {
-        await mongoose.connect('mongodb://uhgadps8czlfzbpovalo:Rm2DDVlJMdIyzFKf8sPd@bfpqubni11nmfa0-mongodb.services.clever-cloud.com:27017/bfpqubni11nmfa0', {
+        await mongoose.connect('mongodb://uhgadps8czlfzbpovalo:Rm2DDVlJMdIyzFKf8sPd@n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017,n2-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017/bfpqubni11nmfa0?replicaSet=rs0', {
             'useNewUrlParser': true,
             'useUnifiedTopology': true
         })
